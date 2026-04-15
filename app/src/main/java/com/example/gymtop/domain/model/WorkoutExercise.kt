e257ef7546bf5614ef57dbcca9d3e00518772785
Leandro Giordani <leandro.lgiordani@gmail.com>
2026-04-15 19:36:56 -0300

    fix issue reported at https://github.com/LeandroGiordani/GymTop/pull/3#discussion_r3040386741

--- app/src/main/java/com/example/gymtop/domain/model/WorkoutExercise.kt
+++ app/src/main/java/com/example/gymtop/domain/model/WorkoutExercise.kt
@@ -3,7 +3,5 @@ package com.example.gymtop.domain.model
 data class WorkoutExercise(
     val id: Long,
     val exercise: Exercise,
-    val sets: Int,
-    val exerciseSet: List<SetType>,
-    val weight: Double?
+    val set: List<SetType>,
 )
